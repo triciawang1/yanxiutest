8776b731dd070ae43b2700e28443e05c460e613e
wangxiaocui <wangxiaocui@yanxiu.com>
2017-02-10 11:31:33 +0800

    调整期望结果

--- src/test/resources/rule_mock.js
+++ src/test/resources/rule_mock.js
@@ -7,7 +7,7 @@ module.exports = {
     //mark if use local response
     shouldUseLocalResponse : function(req,reqBody){
 //var uris=new Array(/initialize/,/login.json/,/getEditUserInfo/,/trainlist/,/examine/,/noticeList/,/briefList/,/taskList/);
-var uris=new Array(/login.json/,/getEditUserInfo/,/trainlist/,/examine/,/noticeList/,/briefList/,/taskList/,/homeworkList/,/myCourseList/,/checkedMobileUser/);
+var uris=new Array(/login.json/,/getEditUserInfo/,/trainlist/,/examine/,/noticeList/,/briefList/,/taskList/,/homeworkList/,/myCourseList/,/checkedMobileUser/,/courselist/);
 for(var i = 0, l = uris.length; i < l; i++) 
 		{
  if(uris[i].test(req.url)){
@@ -27,7 +27,7 @@ for(var i = 0, l = uris.length; i < l; i++)
         var client = request.createClient('http://127.0.0.1:8789');
 		
 //var uris=new Array("/initialize","/login.json","/getEditUserInfo","/trainlist","/examine","/noticeList","/briefList","/taskList");
-var uris=new Array("/login.json","/getEditUserInfo","/trainlist","/examine","/noticeList","/briefList","/taskList","/homeworkList","/myCourseList","/checkedMobileUser");
+var uris=new Array("/login.json","/getEditUserInfo","/trainlist","/examine","/noticeList","/briefList","/taskList","/homeworkList","/myCourseList","/checkedMobileUser","/courselist");
 
         client.get(uris[req.replaceLocalFile], function(err, res, body)  //触发mock，到moco中获取mock响应数据
                 {
